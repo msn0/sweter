8dffbc4910d8cc7a1a8b14a6b658558f2c7c1ce1
Michał Jezierski <ambinanitelo@gmail.com>
2015-03-18 15:46:57 +0100

    fake test

---
+++ test/test.js
@@ -0,0 +1,10 @@
+/* global describe, it */
+'use strict';
+
+var assert = require('assert');
+
+describe('Sweter', function () {
+  it('should be truthy', function () {
+    assert.equal(true, true);
+  });
+});
