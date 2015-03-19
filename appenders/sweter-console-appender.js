be7f96d845d21e73dd4a72c74242769aa3569279
Michał Jezierski <ambinanitelo@gmail.com>
2015-03-19 15:02:56 +0100

    getting things to do at least something

--- appenders/sweter-console-appender.js
+++
@@ -1,5 +0,0 @@
-module.exports.push = function(timestamp, data) {
-    console.log(
-        (new Date()).toDateString() + " | " + JSON.stringify({"data":"data"})
-    );
-}
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 'use strict';
 
+var phantomas = require('phantomas');
+var appender = require('./lib/appenders/sweter-console-appender');
+
 module.exports.process = function (url) {
-  console.log(url);
+  phantomas(url, function(err, json, results) {
+    appender.push(new Date().getTime(), json.metrics.domInteractive);
+  });
 };
